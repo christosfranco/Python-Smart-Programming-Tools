6be80d505150568b2e99b2e40e3faa9c3f01d218
marcusTm <marcusmathiesen@yahoo.dk>
2020-06-04 23:02:20 +0200

    added scrapped at to wiki dataset

--- WikinewsFragment/wikitest/spiders/wiki.py
+++ WikinewsFragment/wikitest/spiders/wiki.py
@@ -1,78 +1,69 @@
 import scrapy
 import re
-# Import the CrawlerProcess: for running the spider
-from scrapy.crawler import CrawlerProcess
+import datetime 
+
+scrapped_at = datetime.datetime.now()
+
 class testSpider(scrapy.Spider):
     name = "wiki"
+
     def start_requests(self):
         start_urls = [
             "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=D",
-            '''"https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=E",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=F",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=G",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=H",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=I",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=J",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=K",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=L",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=M",
-            "https://en.wikinews.org/w/index.php?title=Category:Politics_and_conflicts&from=N",'''
         ]
+
+
+        #yield all the article links from the start urls
         for url in start_urls:
             yield scrapy.Request( url = url, callback = self.parse )
-
+        
+        
+    #We want to follow the next page from the start urls until  we are finished entirely (at letter O)
     #Will find all links in the start url that correspond to category
-    #Might also find some categories that should not be processed
-    #maybe use some regex to filter them
-    def parse(self, response):
-        links = response.css('div.mw-category-group ul li').xpath('a/@href').extract()
+    #Might also find some categories that should not be processed --- Fixed no longer applicable
+    def parse(self, response):    
+        links = response.xpath('/html/body/div[3]/div[3]/div[4]/div[2]/div[2]/div/div/div/ul/li/a/@href').extract()
+        WantedArticles = r"/wiki/[D-N]"
+        #links = response.css('div.mw-category-group ul li').xpath('a/@href').extract()
         for link in links:
-            if link:
+            if link and (re.match(WantedArticles , link ) != None):
                 yield response.follow(url = link, callback = self.parse2)
 
+        #Change the last in the string to the last letter
+        forbiddenNextPage =  r"https:\/\/en\.wikinews\.org\/w\/index\.php\?title=Category:Politics_and_conflicts&pagefrom=O"
+        #This has to be get() instead of extract so that forbiddenNextPage in nextpageurl can be compared as strings
+        nextpageurl1 = response.xpath("//*[@id='mw-pages']/a[contains(.,'next page')]/@href").get()
+        nextpageurl = response.urljoin(nextpageurl1)
+
+        if nextpageurl and (re.match(forbiddenNextPage , nextpageurl ) == None):
+            # If we've found a pattern which matches
+            #print("Found url: {}".format(nextpageurl)) # Write a debug statement
+            yield scrapy.Request(nextpageurl, callback=self.parse) # Return a call to the function "parse"
+
     #Will run over all individual articles and extract appropriate data
-    def parse2(self, response):
-        #return individual article 
-        # title
+    def parse2(self, response): #return individual article
+        
         title = response.xpath('/html/body/div[3]/h1/text()').get()
-        #source(s)
-        source = response.xpath('/html/body/div[3]/div[3]/div[4]/div/ul/li/span/text()').getall()
-        #date
-        date = response.xpath('/html/body/div[3]/div[3]/div[4]/div/p[1]/strong/text()').get()
-        #text
-        text = response.xpath('/html/body/div[3]/div[3]/div[4]/div/p/text()').getall()
-        #put the date and text in the zip
-    
-        if title:
-            yield {
-                        'title': title,
-                    }
-        if source:
-            yield {
-                        'source': source,
-                    }
-        if date:
-            yield {
-                        'date': date,
-                    }
-        if text:
-            yield {
-                        'text': text,
-                    }
-        #for crs_title, crs_descr, text in zip( title, source , text ):
-            #put date text and source in right side of equal sign
-            #title as key
-        #test_dict[title] = [source , date , text]
+        
+        source = response.xpath('/html/body/div[3]/div[3]/div[4]/div/ul/li/span/descendant-or-self::text()').extract()
+        
+        text = response.xpath("""/html/body/div[3]/div[3]/div[4]/div/p/descendant-or-self::text()[not( parent::strong | ancestor::i)]""").extract()
 
-#dictionary to store data
-#test_dict = dict()
+        keywords = response.xpath("""//div[contains(@class, 'mw-normal-catlinks')]/ul/descendant-or-self::text()""").extract()
 
-#run spider
-#process = CrawlerProcess()
-#process.crawl(testSpider)
-#process.start()
-#print(test_dict.keys())
+        article_date = keywords[0] #The date at which the article was updated last
 
-# Print a preview of courses
-#var = test_dict.keys()
-#print(var)
+        keywords.pop(0)
+
+        finaltext = ''.join(text)
+
+        finalsource = ''.join(source)
+
+        yield {
+            'title'        : title,
+            'source'       : finalsource,
+            'scrapped_at'  : scrapped_at,
+            'text'         : finaltext,
+            'keywords'     : keywords,
+            'article_date' : article_date,
+        }
